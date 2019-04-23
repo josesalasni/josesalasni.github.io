0576cd42a748f49702e2e44d0bf14d71bb2e8b93
Carlos José Salas <josesalasni@gmail.com>
2019-04-23 17:33:39 -0500

    menu

--- App.js
+++ App.js
@@ -4,30 +4,60 @@ const mySiema = new Siema({
     loop: true,
     easing: "cubic-bezier(0.68, -0.55, 0.27, 1.55)",
 });
-  document.querySelector('.prev').addEventListener('click', function  (e) {
+
+document.querySelector('.prev').addEventListener('click', function  (e) {
     e.preventDefault();
     mySiema.prev();
-  });
-  document.querySelector('.next').addEventListener('click', function (e) {
+});
+  
+document.querySelector('.next').addEventListener('click', function (e) {
     e.preventDefault();
     mySiema.next();
-  }); 
+}); 
 
  
 function AboutMe () {
+    closeNav();
+    
     document.getElementById('about-me').scrollIntoView({
         behavior: 'smooth'
-    });
+    });    
 }
 
 function Projects () {
+    closeNav();
+    
     document.getElementById('projects').scrollIntoView({
         behavior: 'smooth'
     });
 }
 
 function Technologies (){
+    closeNav();
+
     document.getElementById('tech').scrollIntoView({
         behavior: 'smooth'
     });
 }
+
+/* Back to top */
+function start() {
+    document.getElementById('maincontent').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+  
+function openNav() {
+    window.addEventListener("scroll", noscroll);
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function noscroll() {
+    window.scrollTo(0, 0);
+}
+  
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    window.removeEventListener("scroll", noscroll);
+    document.getElementById("myNav").style.height = "0%";
+} 
