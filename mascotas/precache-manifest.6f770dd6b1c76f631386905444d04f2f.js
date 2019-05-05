18776052cac3bf5124ac01a4b1b98a95328ef4ae
Carlos José Salas <josesalasni@gmail.com>
2019-05-05 13:46:51 -0500

    Updates

---
+++ mascotas/precache-manifest.6f770dd6b1c76f631386905444d04f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e388dabab8cd827c8124465e12b2ac",
+    "url": "/mascotas/index.html"
+  },
+  {
+    "revision": "b53252b984d3872f13dd",
+    "url": "/mascotas/static/css/2.d4be0b98.chunk.css"
+  },
+  {
+    "revision": "43bb43bbec8a9c7f4c02",
+    "url": "/mascotas/static/css/main.31cd880f.chunk.css"
+  },
+  {
+    "revision": "b53252b984d3872f13dd",
+    "url": "/mascotas/static/js/2.2978bdfc.chunk.js"
+  },
+  {
+    "revision": "43bb43bbec8a9c7f4c02",
+    "url": "/mascotas/static/js/main.4ec8bbba.chunk.js"
+  },
+  {
+    "revision": "aaa1f773f54c1deb15be",
+    "url": "/mascotas/static/js/runtime~main.594e2853.js"
+  },
+  {
+    "revision": "3150a290383b2a36f1a2652c5c229904",
+    "url": "/mascotas/static/media/logo.3150a290.png"
+  }
+]);
