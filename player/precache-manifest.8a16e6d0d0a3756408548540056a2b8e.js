fa7395bb3250d13de09ff035cdbc1c8ed4a104b7
Carlos José Salas <josesalasni@gmail.com>
2019-07-05 10:34:02 -0600

    test

---
+++ player/precache-manifest.8a16e6d0d0a3756408548540056a2b8e.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = [
+  {
+    "revision": "c951410dbc4a5c6cbd5c",
+    "url": "styles.css"
+  },
+  {
+    "url": "main.bca5a4b70ca7ffa5aa78.bundle.js"
+  },
+  {
+    "url": "da4ea5cdfca6b3baab285741f5ccb59f.ttf"
+  },
+  {
+    "url": "96c476804d7a788cc1c05351b287ee41.eot"
+  },
+  {
+    "url": "29b882f018fa6fe75fd338aaae6235b8.woff"
+  },
+  {
+    "url": "0509ab09c1b0d2200a4135803c91d6ce.woff2"
+  }
+];
