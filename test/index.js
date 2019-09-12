e1be3970324685ead55288172c326dd6642f399d
Carlos José Salas <josesalasni@gmail.com>
2019-09-11 22:44:18 -0600

    s

--- test/index.js
+++ test/index.js
@@ -45,7 +45,7 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
+  startEvent: 'DOMContentLoaded', // n
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   //offset: 120, // offset (in px) from the original trigger point
